30028c9508e2987a76f206f6ba302df58e22f721
Aleksei Novikov <novikov.aleksei.92@gmail.com>
2021-11-15 15:17:57 +0200

    Extracted a function

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,8 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..101);
     loop {
         println!("Please input your guess.");
-        let mut guess = String::new();
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line!");
-        println!("You guessed: {}", guess);
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
+        let guess: u32 = read_and_parse_line();
+
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
@@ -23,5 +16,20 @@ fn main() {
                 break;
             },
         }
+    }
+}
+
+fn read_and_parse_line() -> u32 {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line!");
+
+    return match input.trim().parse(){
+        Ok(num) => num,
+        Err(_) => {
+            println!("You typed not a number! Try one more time, please!");
+            return read_and_parse_line()
+        },
     }
 }
